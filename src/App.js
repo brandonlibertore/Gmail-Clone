7e36ebc384922f7941f3228471afe296e88f9efa
Brandon <bliberto@uci.edu>
2022-09-26 14:50:16 -0700

    Header Component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     <div className="app">
       <Header />
-      <h1>Let's Build Gmail</h1>
     </div>
   );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,49 @@
 import React from "react";
 import "./Header.css";
 
+// ICONS
+import MenuIcon from "@mui/icons-material/Menu";
+import SearchIcon from "@mui/icons-material/Search";
+import IconButton from "@mui/material/Button";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import AppsIcon from "@mui/icons-material/Apps";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+
 export default function Header() {
   return (
     <div className="header">
-      <h2>This is a h2</h2>
+      <div className="header__left">
+        <IconButton>
+          <MenuIcon className="icon__color" />
+        </IconButton>
+        <img
+          src="https://i.pinimg.com/originals/ae/47/fa/ae47fa9a8fd263aa364018517020552d.png"
+          alt=""
+          className="logo__img"
+        />
+      </div>
+      <div className="header__middle">
+        <SearchIcon className="icon__color" />
+        <input
+          type="text"
+          placeholder="Search mail"
+          className="header__input"
+        />
+        <ArrowDropDownIcon className="header__input--caret icon__color" />
+      </div>
+      <div className="header__right">
+        <IconButton>
+          <AppsIcon className="icon__color" />
+        </IconButton>
+        <IconButton>
+          <NotificationsIcon className="icon__color" />
+        </IconButton>
+        <AccountCircleIcon
+          fontSize="large"
+          className="avatar__logo icon__color"
+        />
+      </div>
     </div>
   );
 }
