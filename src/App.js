b05b36387ef7f446c55406c2713b1ba744c9e49a
Brandon <bliberto@uci.edu>
2022-09-26 14:16:28 -0700

    Initialized Respository with React dependencies

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <h1>Let's Build Gmail</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <h2>This is a h2</h2>
+    </div>
+  );
+}
